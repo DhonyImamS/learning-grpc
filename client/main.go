9313aa2779e0fab6368dffd4a418703e2c4c997b
DhonyImamSaputra <dhony.imam@bukalapak.com>
2022-02-10 14:57:17 +0700

    first commit

---
+++ client/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"log"
+	"google.golang.org/grpc"
+	"learn_grpc/common/model"
+	"learn_grpc/common/config"
+	"github.com/golang/protobuf/ptypes/empty"
+	"encoding/json"
+  )
+
+func serviceUser() model.UsersClient {
+    port := config.SERVICE_USER_PORT
+    conn, err := grpc.Dial(port, grpc.WithInsecure())
+    if err != nil {
+        log.Fatal("could not connect to", port, err)
+    }
+
+    return model.NewUsersClient(conn)
+}
+
+
+func serviceGarage() model.GaragesClient {
+	port := config.SERVICE_GARAGE_PORT
+	conn, err := grpc.Dial(port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("could not connect to", port, err)
+	}
+ 
+	return model.NewGaragesClient(conn)
+ }
+
+
+ func main() {
+	user1 := model.User{
+		  Id:       "n001",
+		  Name:     "Noval Agung",
+		  Password: "kw8d hl12/3m,a",
+		  Gender:   model.UserGender(model.UserGender_value["MALE"]),
+	  }
+  
+	garage1 := model.Garage{
+		  Id:   "q001",
+		  Name: "Quel'thalas",
+		  Coordinate: &model.GarageCoordinate{
+			  Latitude:  45.123123123,
+			  Longitude: 54.1231313123,
+		  },
+	  }
+
+	garageUserId := model.GarageUserId{
+		UserId: "u00g1",
+	}
+
+	garageAndUserId := model.GarageAndUserId{
+		UserId: "u00g1",
+		Garage: &garage1,
+	}
+	
+
+	user := serviceUser()
+	garage := serviceGarage()
+  
+	// invoke method for register rpc with data user1
+	user.Register(context.Background(), &user1)
+
+	// show all registered users
+	res1, err := user.List(context.Background(), new(empty.Empty))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	res1String, _ := json.Marshal(res1.List)
+	log.Println(string(res1String))
+
+	// invoke add garage through grpc
+	garage.Add(context.Background(), &garageAndUserId)
+
+	// invoke list garage through grpc
+	res2, err := garage.List(context.Background(), &garageUserId)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	res2String, _ := json.Marshal(res2.List)
+	log.Println(string(res2String))
+  }
---
+++ go.mod
@@ -0,0 +1,22 @@
+module learn_grpc
+
+go 1.16
+
+require (
+	github.com/antihax/optional v1.0.0 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cncf/udpa/go v0.0.0-20201120205902-5459f2c99403 // indirect
+	github.com/cncf/xds/go v0.0.0-20210312221358-fbca930ec8ed // indirect
+	github.com/envoyproxy/go-control-plane v0.9.8 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.14.5 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/sys v0.0.0-20220207234003-57398862261d // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/grpc v1.27.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
