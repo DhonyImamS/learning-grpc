0dd5c5981728b134208f19997f1212f081a5d8c0
DhonyImamSaputra <dhony.imamsaputra@larisin.id>
2022-09-21 08:26:33 +0700

    feat: add test list garage

--- integration_test/rpctest/garage_test.go
+++ integration_test/rpctest/garage_test.go
@@ -45,4 +45,39 @@ func TestGarageRPC(t *testing.T) {
     assert.Equal(t, nil, err)
     assert.Equal(t, "", resp.String())
   })
+
+  t.Run("it should return new data garage on fetchList garage after succeed registration", func(t *testing.T) {
+    garageNew := model.Garage{
+      Id:   "q001",
+      Name: "Test Bersama Mba Ayu",
+      Coordinate: &model.GarageCoordinate{
+      Latitude:  45.123123123,
+        Longitude: 54.1231313123,
+      },
+    }
+  
+    payload := model.GarageAndUserId{
+      UserId: "u00g1",
+      Garage: &garageNew,
+    }
+  
+    // invoke add garage through grpc
+    resp, err := rpc.SendRegisterGarage(&payload)
+
+    assert.Equal(t, nil, err)
+    assert.Equal(t, "", resp.String())
+
+    // assertion on fetchList
+    payloadFetch := model.GarageUserId{
+      UserId: payload.UserId,
+    }
+  
+    // invoke list garage through grpc
+    respFetch, err := rpc.FetchListGarage(&payloadFetch)
+    assert.Equal(t, nil, err)
+
+    for index := range respFetch.List {
+      assert.Equal(t, garageNew.String(), respFetch.List[index].String())
+    }
+  })
 }
